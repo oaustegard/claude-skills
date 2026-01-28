23dffbeaf4c99dc53e7de80624596db12f136f6b
Oskar Austegard <oskar@austegard.com>
2026-01-27 21:11:09 -0500

    Modify apt update to ignore errors
    Suppress errors during the apt update command.

--- githubbing/scripts/install-gh.sh
+++ githubbing/scripts/install-gh.sh
@@ -14,7 +14,7 @@ mkdir -p -m 755 /etc/apt/sources.list.d
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
     | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
-apt update -qq
+apt update -qq 2>/dev/null || true
 apt install gh -y -qq
 
 echo "gh $(gh --version | head -1 | awk '{print $3}') installed successfully"
